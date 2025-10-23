89f08fa5d2bb1b990fa710c0f291dece2b281fa7
Eva <tlox_xaxaxa@mail.ru>
2025-10-23 20:35:04 +0300

    add code for pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  base: "/kalyanbomzhh",
   build: {
     // Добавь эти настройки
     cssCodeSplit: true,
